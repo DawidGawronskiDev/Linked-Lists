2c534a7649b7d640268d3d28e8c61f7f6d251b11
Dawid <dawidgawronskicontact@gmail.com>
2023-06-05 22:58:22 +0100

    Add Node and LinkedList Classes

---
+++ linkedLists.js
@@ -0,0 +1,13 @@
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = next;
+  }
+}
+
+class linkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+  }
+}
